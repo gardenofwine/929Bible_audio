053f18b83da2e9e49118caf4f3f877f23156c674
Benny Weingarten-Gabbay <gardenofwine@gmail.com>
2015-05-29 18:17:30 +0300

    Add week and book review posts

--- bible929_load_jsons.rb
+++ bible929_load_jsons.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'json'
+require 'fileutils'
 
 WEEKLY_SUMMARY_START_INDEX = 1000
 BOOK_SUMMARY_START_INDEX = 2000
@@ -7,10 +8,16 @@ BOOK_SUMMARY_START_INDEX = 2000
 # Genesis, Exodus, Leviticus, Numbers, Deuteronomy
 CHAPTERS_PER_BOOK = [50, 40, 27, 36, 34]
 JSON_DIR = "json_downloaded"
-CHAPTER_POST_FILENAME_TEMPLATE = "book_%s_cha_%s_post_%s.json"
-WEEKLY_SUMMARY_POST_FILENAME_TEMPLATE = "book_%s_cha_%s_weeksum_post_%s.json"
-BOOK_SUMMARY_POST_FILENAME_TEMPLATE = "book_%s_summary_post_%s.json"
+MEDIA_DIR = "media_downloaded"
+CHAPTER_POST_FILENAME_TEMPLATE = "book_%.2i_cha_%.3i_post_%s.json"
+WEEKLY_SUMMARY_POST_FILENAME_TEMPLATE = "book_%.2i_cha_%.3i_weeksum_post_%s.json"
+BOOK_SUMMARY_POST_FILENAME_TEMPLATE = "book_%.2i_summary_post_%s.json"
+BOOK_CHAPTER_AUDIO_FILENAME_TEMPLATE = "book_%.2i_cha_%.3i_audio.mp3"
 
+
+POST_FILENAME_REGEX = /book_(\d*)_cha_(\d*)_post_(\d*).json/
+WEEKLY_POST_FILENAME_REGEX = /book_(\d*)_cha_(\d*)_weeksum_post_(\d*).json/
+BOOK_SUMMARY_POST_FILENAME_REGEX = /book_(\d*)_summary_post_(\d*).json/
 
 def book_by_chapter(chapter)
 	chapter_count = 0
@@ -61,30 +68,76 @@ def download_book_summary_posts(index, book)
 	download_posts("#{summury_preview_url_template}#{index}", BOOK_SUMMARY_POST_FILENAME_TEMPLATE % [book, "%s"])
 end
 
-Dir.mkdir JSON_DIR unless File.exist? JSON_DIR
-# Fetch chpater posts
-chapter_base = 1
-CHAPTERS_PER_BOOK.each_with_index do |num_of_chapters, book_idx|
-	current_chapter = 0
-	num_of_chapters.times do |chapter_idx|
-		current_chapter = chapter_base + chapter_idx
-		download_posts_for_chapter(current_chapter, book_idx + 1)
-		if chapter_idx + 1 == num_of_chapters
-			# download book summary
-			puts "**== book summary"
-			download_book_summary_posts(BOOK_SUMMARY_START_INDEX + book_idx + 1, book_idx + 1)
-		elsif current_chapter % 5 == 0
-			# download week summary
-			puts "**== week summary"
-			download_weekly_summary_posts(WEEKLY_SUMMARY_START_INDEX + current_chapter / 5, current_chapter, book_idx + 1)
+
+def download_jsons
+	Dir.mkdir JSON_DIR unless File.exist? JSON_DIR
+	# Fetch chpater posts
+	chapter_base = 1
+	CHAPTERS_PER_BOOK.each_with_index do |num_of_chapters, book_idx|
+		current_chapter = 0
+		num_of_chapters.times do |chapter_idx|
+			current_chapter = chapter_base + chapter_idx
+			download_posts_for_chapter(current_chapter, book_idx + 1)
+			if chapter_idx + 1 == num_of_chapters
+				# download book summary
+				puts "**== book summary"
+				download_book_summary_posts(BOOK_SUMMARY_START_INDEX + book_idx + 1, book_idx + 1)
+			elsif current_chapter % 5 == 0
+				# download week summary
+				puts "**== week summary"
+				download_weekly_summary_posts(WEEKLY_SUMMARY_START_INDEX + current_chapter / 5, current_chapter, book_idx + 1)
+			end
 		end
+		chapter_base = current_chapter + 1
 	end
-	chapter_base = current_chapter + 1
 end
 
-# video_url = post_content['embeddedVideo']
-# puts "ch #{chapter}. post #{post_id}. video #{video_url}"
+def youtube_filename_by_json_file(file_path)
+	filename = file_path.sub(".json", "").sub("#{JSON_DIR}/", "")
+	"#{MEDIA_DIR}/youtube/#{filename}" 
+end
 
-# say -v carmit -r 120 -o "audiofile.aiff" -f "textfile.rtf"
+def process_post_json(file_path)
+	output_filename = youtube_filename_by_json_file(file_path)
+	return if File.exist? output_filename
+	content = File.read file_path
+	post_json =	JSON.parse(content)
+	if post_json['embeddedVideo'] != nil &&  post_json['embeddedVideo'].length > 0
+		system 'youtube-dl', '-o', "#{output_filename}_b.mp3", post_json['embeddedVideo'], '-x', '--audio-format', 'mp3'
+		if (post_json['title'].length > 0)
+			system 'say', '-v', 'carmit', '-r', '120', '-o', "#{output_filename}_a.aiff", post_json['title']
+			system 'lame', '-m', 'm', "#{output_filename}_a.aiff", "#{output_filename}_a.mp3"
+			File.delete "#{output_filename}_a.aiff"
+		else
+			File.open  "#{output_filename}_a_missing_title.txt", 'w' do |file|
+				file.write post_json['embeddedVideo']
+			end
+		end
+	end
+end
 
-# youtube-dl http://www.youtube.com/watch\?v\=xq5-u8wvLo8 -x --audio-format mp3
+def download_youtubes
+	#Download videos and chapters
+	Dir["#{JSON_DIR}/*"].each_with_index do |filename, index|
+		process_post_json(filename)
+	end
+end
+
+def layout_cd_files
+	Dir["#{JSON_DIR}/*"].each_with_index do |filename, index|
+		match = filename.match /book_([\d]*)_cha_([\d]*)/
+		next unless match
+		book, chapter = match.captures
+
+		#copy chapter audio 
+		source_chapter_audio_file = "#{MEDIA_DIR}/chapters/#{chapter.to_i}.mp3"
+		output_chapter_audio_file = "output/#{BOOK_CHAPTER_AUDIO_FILENAME_TEMPLATE % [book.to_i, chapter.to_i]}"
+		if !File.exist? output_chapter_audio_file
+			FileUtils.copy source_chapter_audio_file, output_chapter_audio_file
+		end
+	end
+end
+
+# download_jsons
+# download_youtubes
+layout_cd_files
